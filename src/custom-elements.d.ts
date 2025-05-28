47de99dd66cb246c7c9ea2b1cd157d7003e6a92d
kennytimmer <57355690+kennytimmer@users.noreply.github.com>
2025-05-28 05:48:31 +0200

    gh-pages

--- src/custom-elements.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'md-navigation-rail': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { ref?: any };
-    'md-navigation-rail-item': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { ref?: any };
-  }
-} 
